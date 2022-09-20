b1959208e3a85e5f49b13be2b157e60490008e88
Flurin Brühwiler <47397416+FlurinBruehwiler@users.noreply.github.com>
2022-09-20 09:35:45 +0200

    feat(CSS): Im loving it!!!!

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
-      acme: ["Acme", "'sans-serif'"],
+      acme: ["'Acme'", "sans-serif"],
     },
     extend: {
       colors: {
